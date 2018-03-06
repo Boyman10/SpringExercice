81c7af76de62c8300bea3a2bf390907e3551092d
Boyman10 <contact@yasi.fr>
2018-03-06 21:52:41 +0100

    Working on ticketbatch

--- ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
+++ ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
@@ -4,6 +4,7 @@ package org.example.demo.ticket.batch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.example.demo.ticket.model.exception.TechnicalException;
+import org.springframework.context.ApplicationContext;
 
 
 /**
@@ -24,6 +25,11 @@ public class Main {
      * @throws TechnicalException sur erreur technique
      */
     public static void main(String[] pArgs) throws TechnicalException {
+    	
+    	ApplicationContext vApplicationContext = new ApplicationContext("classpath:/applicationContext.xml");
+    	ManagerFactory vManagerFactory 
+    	= vApplicationContext.getBean("managerFactory",ManagerFactory.class);
+    	
         try {
             if (pArgs.length < 1) {
                 throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
