70f0c133ed1b088bce18af2b15bd246db3060534
Boyman10 <contact@yasi.fr>
2018-05-06 11:37:41 +0200

    Working on DAO with NamedParameterJdbc Template

--- ticket-consumer/src/main/java/org/example/demo/ticket/consumer/contract/dao/TicketDao.java
+++ ticket-consumer/src/main/java/org/example/demo/ticket/consumer/contract/dao/TicketDao.java
@@ -1,6 +1,13 @@
 package org.example.demo.ticket.consumer.contract.dao;
 
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
 
+/**
+ * Interface contract for TicketDao
+ * @author bill
+ *
+ */
 public interface TicketDao {
 
+	public int getCountTicket(RechercheTicket rTicket);
 }
--- ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/TicketDaoImpl.java
+++ ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/TicketDaoImpl.java
@@ -1,8 +1,37 @@
 package org.example.demo.ticket.consumer.impl.dao;
 
+import javax.inject.Named;
+
 import org.example.demo.ticket.consumer.contract.dao.TicketDao;
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Named
 public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao{
+
+	@Override
+	public int getCountTicket(RechercheTicket rTicket) {
+		
+        StringBuilder vSQL = new StringBuilder("SELECT COUNT(*) FROM ticket WHERE 1=1");
+		
+		NamedParameterJdbcTemplate nParamTemplate = new NamedParameterJdbcTemplate(getDataSource());
+        MapSqlParameterSource vParams = new MapSqlParameterSource();
+        
+        
+        if (rTicket != null) {
+            if (rTicket.getAuteurId() != null) {
+                vSQL.append(" AND auteur_id = :auteur_id");
+                vParams.addValue("auteur_id", rTicket.getAuteurId());
+            }
+            if (rTicket.getProjetId() != null) {
+                vSQL.append(" AND projet_id = :projet_id");
+                vParams.addValue("projet_id", rTicket.getProjetId());
+            }
+        }
+
+	
+		return nParamTemplate.queryForObject(vSQL, vParams, Integer.class);
+	}
 
 }
